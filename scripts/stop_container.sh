01559d32dff5271fd0c31985e66aaf440ea46f6e
abheelasha <kiran.abheelasha@gmail.comgit config --list>
2025-10-03 11:08:15 +1000

    updated the stop container script file

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -5,5 +5,5 @@ set -e
 #stop the running container if any
 
 echo "Hi There!"
-container_id = `docker ps | awk -F " " '{print $1}'`
-docker rm -f $container_id
+#containerid=`docker ps | awk -F " " '{print $1}'`
+#docker rm -f $containerid
